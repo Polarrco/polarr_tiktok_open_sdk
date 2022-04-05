4f6e8a93c2bad58437ab377e118ab60e2fdbae96
Daniel Vershinin <d@sgml.me>
2022-04-06 00:34:19 +0300

    TikTok Open SDK as a package.

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "TikTokOpenSDK",
+    platforms: [.iOS(.v13)],
+    products: [
+        .library(
+            name: "TikTokOpenSDK",
+            targets: ["TikTokOpenSDKCore"])
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(
+            name: "TikTokOpenSDK",
+            dependencies: []),
+        .binaryTarget(name: "TikTokOpenSDKCore",
+                      path: "Resources/TikTokOpenSDKCore.xcframework")
+    ]
+)
